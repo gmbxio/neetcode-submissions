2797a420ee1f2550ccf4eefe05242f5e54714da8
Golam Masum Basar <gmbasr@gmail.com>
2026-08-03 23:51:35 +0530

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        result = []
+    
+        def solve(idx: int, currTarget: int, path : List[int]) -> None:
+            if currTarget == 0:
+                result.append(path.copy())
+                return 
+
+            if idx == len(nums) or currTarget < 0:
+                return
+
+            if nums[idx] <= currTarget:
+                path.append(nums[idx])
+                solve(idx, currTarget - nums[idx], path)
+                path.pop()
+
+            solve(idx + 1, currTarget, path)
+        
+        solve(0, target, [])
+        return result
