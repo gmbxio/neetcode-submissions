cbee784e9eb85226787a280c05064309b4d8dd25
Golam Masum Basar <gmbasr@gmail.com>
2026-08-04 00:05:43 +0530

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        parenthesisList = []
+
+        def generateComb(open: int, close: int, curr_str: str):
+
+            if open == n and close == n:
+                parenthesisList.append(curr_str)
+                return 
+            
+            if open < n:
+                generateComb(open + 1, close, curr_str + "(")
+
+            if close < open:
+                generateComb(open, close + 1, curr_str + ")")
+            
+        generateComb(0, 0, "")
+        return parenthesisList
