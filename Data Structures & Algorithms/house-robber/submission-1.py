fc9a103519599de600f9be45c4639b8c3eb2bc2d
Golam Masum Basar <gmbasr@gmail.com>
2026-08-07 12:49:19 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+            
+        rob1, rob2 = nums[0], max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            nextMax = max((nums[i] + rob1), rob2)
+            rob1 = rob2
+            rob2 = nextMax
+        
+        return rob2
