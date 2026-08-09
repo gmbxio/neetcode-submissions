4c3a6ec63f3d760325f3097d79671841bbe976dc
Golam Masum Basar <gmbasr@gmail.com>
2026-08-09 20:03:44 +0530

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        longPal = ""
+
+        def expandFromCentre(l: int, r: int) -> str:
+
+            while l >= 0 and r < n:
+                if s[l] == s[r]:
+                    l -= 1
+                    r += 1
+                else:
+                    break
+            return s[l + 1:r]
+
+        
+        for i in range(n):
+            oddsub = expandFromCentre(i, i)
+            evensub = expandFromCentre(i, i + 1)
+
+            if len(oddsub) > len(longPal):
+                longPal = oddsub
+            if len(evensub) > len(longPal):
+                longPal = evensub
+
+        return longPal
